766b30dea9f644daed9bba4c6d6fe9b9d747bab1
dompling <374779789@qq.com>
2023-04-29 22:05:18 +0800

    fix(update): source

--- src/loading.tsx
+++ src/loading.tsx
@@ -1,13 +1,13 @@
-import { CircularProgress, Stack } from '@mui/material';
+import { CircularProgress, Stack } from "@mui/material";
 
 export default function () {
   return (
     <Stack
-      alignItems={'center'}
-      justifyContent={'center'}
+      alignItems={"center"}
+      justifyContent={"center"}
       sx={{ p: 0, m: 0, minHeight: "100vh", minWidth: `100vw` }}
     >
-      <CircularProgress />
+      <CircularProgress sx={{ mb: 2 }} />
     </Stack>
   );
 }
