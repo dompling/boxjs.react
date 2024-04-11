5ff8465db8b4e61fb38a6ef6f17e0b7b6a9c48c5
dompling <374779789@qq.com>
2024-04-11 13:42:13 +0800

    build(github): update

--- config/config.uat.ts
+++ config/config.uat.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "@umijs/max";
 console.log(`Github`);
 
 export default defineConfig({
-  base: "./boxjs.react/",
-  publicPath: "./boxjs.react/",
+  base: "./",
+  publicPath: "./",
 });
