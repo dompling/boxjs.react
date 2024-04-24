16a68b42bf0a494e9692c71ac39ed98235fc2785
dompling <374779789@qq.com>
2024-04-24 11:15:19 +0800

    style(css): 样式调整

--- src/components/BoxJSActions/index.tsx
+++ src/components/BoxJSActions/index.tsx
@@ -180,7 +180,6 @@ const BoxJSActions: React.FC = () => {
             top: 0,
             right: 0,
             bottom: 0,
-            background: "rgba(0,0,0,.2)",
           }}
         />
       )}
